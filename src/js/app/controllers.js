b93f8c7df5d699466260aa75cb59b06857b39fe2
W8inG4DeatH <lipinski.m@o2.pl>
2017-01-19 18:22:24 +0100

    Home site

--- src/js/app/controllers.js
+++ src/js/app/controllers.js
@@ -1,24 +1,127 @@
 (function(){
-
+  
     var app = angular.module('myControllers', ['myDirectives', 'myServices']);
+  
+    app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
-    app.controller('myController', ['$scope', 'mainService', 'getWWWData', function($scope, mainService, getWWWData) {
+        $routeProvider
+            .when('/home', {
+                controller: 'homeController',
+                templateUrl: 'views/home.html',
+                label: 'Witaj'
+            })
+            .when('/firma', {
+                controller: 'firmaController',
+                templateUrl: 'views/firma.html',
+                label: 'Firma'
+            })
+            .when('/oferta/:categoryName', {
+                controller: 'ofertaController',
+                templateUrl: 'views/oferta.html',
+                label: 'Oferta'
+            })
+            .when('/oferta', {
+                controller: 'ofertaController',
+                templateUrl: 'views/oferta.html',
+                label: 'Oferta'
+            })
+            .when('/pobierz', {
+                controller: 'pobierzController',
+                templateUrl: 'views/pobierz.html',
+                label: 'Pobierz'
+            })
+            .when('/kontakt', {
+                controller: 'kontaktController',
+                templateUrl: 'views/kontakt.html',
+                label: 'Kontakt'
+            })
+            .otherwise({
+                redirectTo: '/home'
+            });            
 
-        getWWWData('www_data', function (data) {
-            $scope.wwwData = data;
-            $scope.WWWInit();
-        });
+        $locationProvider
+            .html5Mode(true);
+            
+    }]);
+ 
+    app.controller('myController', ['$scope', '$http', '$window', '$location', '$anchorScroll', 'mainService', 'getWWWData', function($scope, $http, $window, $location, $anchorScroll, mainService, getWWWData) {
 
-        $scope.WWWInit = function() {
-            $scope.siteDatas = {};
-            angular.forEach($scope.wwwData.content, function(site, siteKey) {
-                $scope.siteDatas[site.name] = site.label;
-            });
-        };
+        $scope.activeBG = 1;
 
         $(window).load(function() {
             $('.siteLoader').hide();
             mainService.StartTooltip();
+        });
+
+        $( window ).resize(function() {
+            $scope.OnWindowResize();
+        });
+
+        $scope.OnWindowResize = function() {
+            var width = $( window ).width();
+            var height = $( window ).height();
+            if ( width > (1280/800) * height ) {
+                $('.bg-image').width(width);
+                $('.bg-image').height(800 * width / 1280);
+            } else {
+                $('.bg-image').height(height);
+                $('.bg-image').width(1280 * height / 800);
+            } 
+        };
+
+        $scope.ScrollSite = function(sectorId) {
+            $location.hash(sectorId);
+            $anchorScroll();
+        };
+
+        $scope.OnWindowResize();
+
+    }]);
+
+    //////////
+    // HOME //
+    //////////
+
+    app.controller('homeController', ['$scope', 'mainService', function($scope, mainService){
+
+        angular.element(document).ready(function() {
+
+        });
+
+    }]);
+
+    ////////////
+    // OFERTA //
+    ////////////  
+
+    app.controller('ofertaController', ['$scope', '$routeParams', 'mainService', function($scope, $routeParams, mainService){
+
+        angular.element(document).ready(function() {
+            //$scope.Animateprodukty();
+        });
+
+    }]);
+
+    /////////////
+    // POBIERZ //
+    /////////////
+
+    app.controller('pobierzController', ['$scope', 'mainService', function($scope, mainService){
+
+        angular.element(document).ready(function() {
+
+        });
+
+    }]);
+
+    /////////////
+    // KONTAKT //
+    /////////////
+
+    app.controller('kontaktController', ['$scope', 'mainService', function($scope, mainService){
+
+        angular.element(document).ready(function() {
+
         });
 
     }]);
